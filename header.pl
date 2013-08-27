67becd8db48c8398eaec731f414b6093b5c314da
Steven Gleed <ledbybyson@gmail.com>
2013-08-27 16:45:50 +0100

    New updated header files

--- header.pl
+++ header.pl
@@ -1,5 +1,9 @@
 #!/usr/bin/perl 
 
+=pod
+    Program to parse a folder of header files relating to the games outcome tables. Merges result into one outcome.h file.
+    Also outputs the relevant data for GameAllowEvent.cpp.
+=cut
 
 use strict;
 use warnings;
@@ -8,10 +12,14 @@ use Cwd;
 use constant false => 0;
 use constant true  => 1;
 
-my %feature_values = (97,"crab3", 115, "wonga3");
+my %feature_values = (97,"crab3", 115, "lobster3", 116, "lobster4",
+    117, "lobster5");
 
 my $outcomes = "outcomes.h";
+my @win_values;
+my $allow_event_cast = "(unsigned char *)" # concat this with the filename
 
+# outcomes_180 is used twice, once for £1.80 and once for wonga4
 sub parse_directory($) {
     my $start_dir = $_[0];
     printf $start_dir, "\n";
@@ -29,7 +37,6 @@ sub parse_directory($) {
         opendir(DIR, $start_dir) or die "Cannot open the directory:
             $start_dir.\n";
 
-        
         while (my $file = readdir(DIR)) {
             next if ($file =~ m/^\./);
 
@@ -37,7 +44,7 @@ sub parse_directory($) {
           
 
             if (&check_extension($full_path)) {
-                print $full_path, "\n";                
+                #print $full_path, "\n";                
                 push(@headers, $full_path) if (-f $full_path);
             }    
         }
@@ -45,10 +52,11 @@ sub parse_directory($) {
         print "number of header files is ", 0+@headers, "\n"; 
         
         if (length(@headers) == 0) {
-            print "Empty directory listing. Find me some header files
+            print "Empty directory listing. Pass me some header files
                 please.\n";
             return;
         }
+                
         closedir(DIR);
     }
     return (@headers);
@@ -68,20 +76,37 @@ sub parse_file($) {
     open FILE, "<$file" or die $!;
     open OUTC_FILE, ">>$outcomes" or die $!;
     my $counter = 1;
-    
-    while (<FILE>) {
-        print $_, "\n";
-        print $counter++, "\n";
+    my @lines = <FILE>; 
+    my $file_counter = 0+@lines;
+    push @win_values, $file_counter;
+
+    for (@lines) {
+        s/L5_Outcome_0+/outcomes_/; 
+        if ($counter == 1) {
+            $file_counter--;
+            s/\[(\d+)\]/[$file_counter]/;
+        }
+        print OUTC_FILE $_;
+        $counter++;
     }
+
+    print OUTC_FILE "\n\n";    
     close FILE or die $!;
     close OUTC_FILE or die $!;
 }
 
-sub header_line_count($) {
-    my $count = 0;
-    return $count;
+sub write_game_allow_event($) {
+    
 }
 
 my @all_files = &parse_directory($ARGV[0]);
-print "key 97 is ", $feature_values{97}, "\n";
-&parse_file($all_files[2]);
+#print "key 97 is ", $feature_values{97}, "\n";
+
+#for my $index (@all_files) {
+for (my $i = 0; $i < 0+@all_files; $i++) {
+    &parse_file($all_files[$i]);
+}
+
+#&parse_file($all_files[2]);
+
+
